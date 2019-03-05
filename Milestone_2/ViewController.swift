da4d62c2f02640882ba98455fbb9efeeb103ffe0
Diwakar Sharma <diwakarsharma95@gmail.com>
2019-03-05 16:26:47 +1000

    adding items to catagories

--- Milestone_2/ViewController.swift
+++ Milestone_2/ViewController.swift
@@ -31,8 +31,90 @@ class ViewController: UIViewController {
     }
     
     @IBAction func Enter(_ sender: Any) {
-        textArea.text = "->\(self.textField.text ?? " ")"
+        //textArea.text = "->\(self.textField.text ?? " ")"
+        let str = self.textField.text
+        if let newStr = str{
+        let newStrArr = newStr.components(separatedBy: String(" "))
+        if newStrArr[0].contains("add"){
+            let category = newStrArr[1]
+            let description = newStrArr[2]
+            let amount = newStrArr[3]
+            textArea.text = "-> Added \(category) \(description) $\(amount)"
+            if category == "Entertainment"{
+                Entertainment[Int(amount)!] = description
+            }else if category == "Shopping"{
+                Shopping[Int(amount)!] = description
+            }else{
+                Education[Int(amount)!] = description
+            }
+        }
     }
+}
+        
+        
+//        while true{
+//            let str = readLine()
+//            if let newStr = str{
+//                let newStrArr = newStr.components(separatedBy: String(" "))     //slice the string from blank spaces
+//                if newStrArr[0].contains("add"){
+//                    let category = newStrArr[1]
+//                    let description = newStrArr[2]
+//                    let amount = newStrArr[3]
+//                    print("Added \(category) \(description) $\(amount)")
+//                    if newStr.contains("Entertainment"){
+//                        Entertainment[Int(amount)!] = description
+//                    }else if newStr.contains("Shopping"){
+//                        Shopping[Int(amount)!] = description
+//                    }else{
+//                        Education[Int(amount)!] = description
+//                    }
+//                    Entertainment.forEach { (k,v) in list[k] = v }
+//                    Education.forEach { (k,v) in list[k] = v }
+//                    Shopping.forEach { (k,v) in list[k] = v }
+//                }
+//                else{
+//                    switch newStr{
+//                    case "total":
+//                        let grandTotal = Array(Entertainment.keys).reduce(0, +) + Array(Shopping.keys).reduce(0, +) + Array(Education.keys).reduce(0, +)
+//                        print("Grand Total \(grandTotal)")
+//                    case "list":
+//                        let sortedAll = Array(list.keys).sorted(by: >)
+//                        for i in 0..<sortedAll.count{
+//                            print(" \(list[sortedAll[i]]!) $\(sortedAll[i])")
+//                        }
+//                    case "total \(newStrArr[1])":
+//                        if newStrArr[1] == "Entertainment"{
+//                            print("Total for Entertainment = $\(Array(Entertainment.keys).reduce(0, +))")
+//                        }else if newStrArr[1] == "Shopping"{
+//                            print("Total for Shopping = $\(Array(Shopping.keys).reduce(0, +))")
+//                        }else if newStrArr[1] == "Education"{
+//                            print("Total for Education = $\(Array(Education.keys).reduce(0, +))")
+//                        }else{
+//                            print("Wrong Category! Choose a different catagory")
+//                        }
+//                    case "list \(newStrArr[1])":
+//                        if newStrArr[1] == "Entertainment"{
+//                            for (key,value) in Entertainment{
+//                                print("Entertainment \(value) $\(key)")
+//                            }
+//                        }else if newStrArr[1] == "Shopping"{
+//                            for (key,value) in Shopping{
+//                                print("Shopping \(value) $\(key)")
+//                            }
+//                        }else if newStrArr[1] == "Education"{
+//                            for (key,value) in Education{
+//                                print("Education \(value) $\(key)")
+//                            }
+//                        }else{
+//                            print("Wrong Category! Choose a different catagory")
+//                        }
+//                    default:
+//                        print("I am in default")
+//                    }
+//                }
+//            }
+//        }
+
     @IBAction func Reset(_ sender: Any) {
     }
     
